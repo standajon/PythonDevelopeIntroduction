50233c2479a5d602e53c59eda57a8dd6ad556b1f
standa <standa.jonak@gmail.com>
2022-09-19 14:45:22 +0200

    files added

---
+++ general/memoryTest.py
@@ -0,0 +1,7 @@
+a="John"
+b=a
+c=123456
+
+print(id(a))
+print(id(b))
+print(id(c))
---
+++ printScripts/Strings.py
@@ -0,0 +1,21 @@
+print("Ahoj svete")
+
+zprava="Mam rad Python"
+print(zprava)
+
+zprava2="Tohle je hodne douhy text.Tohle je hodne douhy text.Tohle je hodne douhy text.Tohle je hodne douhy text.Tohle je hodne douhy text.Tohle je hodne douhy text."
+print(zprava2)
+
+zprava3="""Tohle je hodne douhy text.
+Tohle je hodne douhy text.
+Tohle je hodne douhy text.
+Tohle je hodne douhy text.
+Tohle je hodne douhy text.
+Tohle je hodne douhy text."""
+print(zprava3)
+
+
+print(len(zprava3))
+
+print(zprava[1])
+
