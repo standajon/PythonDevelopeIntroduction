fd8f21d36e260f677434d0e5ed2ab0f9802a4121
standa <standa.jonak@gmail.com>
2022-09-19 13:52:57 +0200

    Initial commit

---
+++ HelloWorld.py
@@ -0,0 +1,10 @@
+print("Hello World")
+print("Hello World")
+print("Hello World")
+print("Hello World") #TODO udelat neco
+print("Hello World")
+print("Hello World")
+print("Hello World") #TODO acascascasc
+print("Hello World")
+print("Hello World")
+
