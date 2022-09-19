bcad441de4284ba77c95660227462c2e89cf15b2
standa <standa.jonak@gmail.com>
2022-09-19 14:09:48 +0200

    ReadMe edited

---
+++ printScripts/Escape.py
@@ -0,0 +1,8 @@
+print("toto je jeden radek")
+print("************************")
+print("A toto jsou\ndva radky")
+print("************************")
+print("A toto je veta se zpetnym lomitkem \\")
+print("Hi, it\'s me")
+
+
