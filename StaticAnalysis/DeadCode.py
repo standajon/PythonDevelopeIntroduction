4381cf3cbd1b9869630130289d38facaae026a17
standa <standa.jonak@gmail.com>
2022-09-27 19:07:08 +0200

    edited

--- StaticAnalysis/DeadCode.py
+++ StaticAnalysis/DeadCode.py
@@ -1,12 +1,4 @@
-#this will not use the 'sum' variable
-def calculate_sum(x, y):
-    sum = x + y
-    return x + y
-
-
-#the sum variable is not used either, however it also causes the compilation error
 def calculate_minus(x, y):
-    sum = x - y
+         sum = x - y
     return x - y
 
-print(calculate_minus(2,5))
